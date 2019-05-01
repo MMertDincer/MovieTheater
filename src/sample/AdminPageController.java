18fe5d89c64b550bd48d2ee90ba57c6065b16c1a
Mert DINCER <mert.dincer05@gmail.com>
2019-05-01 16:23:59 +0300

    Admins can now add and view movies, auditoriums and screenings.

--- src/sample/AdminPageController.java
+++ src/sample/AdminPageController.java
@@ -1,19 +1,214 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 
 public class AdminPageController {
 
+    // Users tab
     public TableView userTableView;
     public TextField usernameField;
     public PasswordField passwordField;
-    public Button registerButton;
+    public Button refreshUserTable;
+    public Button registerUserButton;
     public Button deleteButton;
 
-    DatabaseConnection connection = new DatabaseConnection();
+    // Movies tab
+    public TableView movieTableView;
+    public TextField titleField;
+    public TextField directorField;
+    public TextField castField;
+    public TextField descField;
+    public TextField posterUrlField;
+    public DatePicker releaseDateField;
+    public TextField durationField;
+    public Button addMovieButton;
+    public AnchorPane refreshMovieTable;
+    public Button refreshMovieTableButton;
 
-    public void registerButtonClicked(MouseEvent mouseEvent) {
+    // Auditoriums tab
+    public TableView auditoriumTableView;
+    public Button refreshAuditoriumTableButton;
+    public TextField auditNameField;
+    public TextField auditSeatNoField;
+    public Button addAuditoriumButton;
+
+    // Screenings tab
+    public TableView screeningTableView;
+    public Button refreshScreeningTableButton;
+    public TextField movieIDField;
+    public TextField auditoriumIDField;
+    public Button addScreeningButton;
+    public TextField timestampField;
+
+
+    DatabaseConnection connection = new DatabaseConnection();
+    ObservableList<ObservableList> data;
+
+    public AdminPageController() {
+
+    }
+
+    public void PopulateUsersTable() {
+        data = FXCollections.observableArrayList();
+        ResultSet usersResultSet = connection.GetUserTable();
+
+        // Clear the table first for the refresh
+        userTableView.getItems().clear();
+        userTableView.getColumns().clear();
+
+        try {
+            for (int i = 0; i < usersResultSet.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(usersResultSet.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+
+                userTableView.getColumns().addAll(col);
+                //System.out.println("Column [" + i + "] ");
+            }
+
+            while(usersResultSet.next()){
+                ObservableList<String> row = FXCollections.observableArrayList();
+
+                for(int i = 1 ; i <= usersResultSet.getMetaData().getColumnCount(); i++)
+                    row.add(usersResultSet.getString(i));
+
+                //System.out.println("Row [1] added "+row );
+                data.add(row);
+            }
+
+            userTableView.setItems(data);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    } // end PopulateUsersTable()
+
+    public void PopulateMoviesTable() {
+        data = FXCollections.observableArrayList();
+        ResultSet moviesResultSet = connection.GetMovieTable();
+
+        // Clear the table first for the refresh
+        movieTableView.getItems().clear();
+        movieTableView.getColumns().clear();
+
+
+        try {
+            for (int i = 0; i < moviesResultSet.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(moviesResultSet.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+
+                movieTableView.getColumns().addAll(col);
+                //System.out.println("Column [" + i + "] ");
+            }
+
+            while(moviesResultSet.next()){
+                ObservableList<String> row = FXCollections.observableArrayList();
+
+                for(int i = 1 ; i <= moviesResultSet.getMetaData().getColumnCount(); i++)
+                    row.add(moviesResultSet.getString(i));
+
+                //System.out.println("Row [1] added "+row );
+                data.add(row);
+            }
+
+            movieTableView.setItems(data);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    } // end PopulateMoviesTable()
+
+    public void PopulateAuditoriumTable() {
+        data = FXCollections.observableArrayList();
+        ResultSet auditoriumResultSet = connection.GetAuditoriumTable();
+
+        // Clear the table first for the refresh
+        auditoriumTableView.getItems().clear();
+        auditoriumTableView.getColumns().clear();
+
+
+        try {
+            for (int i = 0; i < auditoriumResultSet.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(auditoriumResultSet.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+
+                auditoriumTableView.getColumns().addAll(col);
+                //System.out.println("Column [" + i + "] ");
+            }
+
+            while(auditoriumResultSet.next()){
+                ObservableList<String> row = FXCollections.observableArrayList();
+
+                for(int i = 1 ; i <= auditoriumResultSet.getMetaData().getColumnCount(); i++)
+                    row.add(auditoriumResultSet.getString(i));
+
+                //System.out.println("Row [1] added "+row );
+                data.add(row);
+            }
+
+            auditoriumTableView.setItems(data);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    } // end PopulateAuditoriumTable()
+
+    public void PopulateScreeningTable() {
+        data = FXCollections.observableArrayList();
+        ResultSet screeningResultSet = connection.GetScreeningTable();
+
+        // Clear the table first for the refresh
+        screeningTableView.getItems().clear();
+        screeningTableView.getColumns().clear();
+
+
+        try {
+            for (int i = 0; i < screeningResultSet.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(screeningResultSet.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+
+                screeningTableView.getColumns().addAll(col);
+                //System.out.println("Column [" + i + "] ");
+            }
+
+            while(screeningResultSet.next()){
+                ObservableList<String> row = FXCollections.observableArrayList();
+
+                for(int i = 1 ; i <= screeningResultSet.getMetaData().getColumnCount(); i++)
+                    row.add(screeningResultSet.getString(i));
+
+                //System.out.println("Row [1] added "+row );
+                data.add(row);
+            }
+
+            screeningTableView.setItems(data);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        PopulateUsersTable();
+        PopulateMoviesTable();
+        PopulateAuditoriumTable();
+        PopulateScreeningTable();
+    }
+
+    public void registerUserButtonClicked(MouseEvent mouseEvent) {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
@@ -27,7 +222,68 @@ public class AdminPageController {
         }
     }
 
-    public void deleteButtonClicked(MouseEvent mouseEvent) {
-        //TODO: Delete user by selected row on the table
+    public void deleteUserButtonClicked(MouseEvent mouseEvent) {
+        //TODO: Delete the selected user
+    }
+
+    public void addMovieButtonClicked(MouseEvent mouseEvent) {
+        String title = titleField.getText();
+        String director = directorField.getText();
+        String cast = castField.getText();
+        String description = descField.getText();
+        String posterUrl = posterUrlField.getText();
+        Date releaseDate = Date.valueOf(releaseDateField.getValue());
+        String duration = durationField.getText();
+
+        if (connection.IsAddMovieSuccessful(title, director, cast, description, posterUrl, releaseDate, duration)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successfully added!");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Something went wrong.");
+            alert.showAndWait();
+        }
+    }
+
+    public void refreshUserTableButtonClicked(MouseEvent mouseEvent) {
+        PopulateUsersTable();
+    }
+
+    public void refreshMovieTableButtonClicked(MouseEvent mouseEvent) {
+        PopulateMoviesTable();
+    }
+
+    public void refreshAuditoriumTableButtonClicked(MouseEvent mouseEvent) {
+        PopulateAuditoriumTable();
+    }
+
+    public void refreshScreeningTableButtonClicked(MouseEvent mouseEvent) {
+        PopulateScreeningTable();
+    }
+
+    public void addAuditoriumButtonClicked(MouseEvent mouseEvent) {
+        String name = auditNameField.getText();
+        String seatsNumber = auditSeatNoField.getText();
+
+        if (connection.IsAddAuditoriumSuccessful(name, seatsNumber)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successfully added!");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Something went wrong.");
+            alert.showAndWait();
+        }
+    }
+
+    public void addScreeningButtonClicked(MouseEvent mouseEvent) {
+        int movieID = Integer.parseInt(movieIDField.getText());
+        int auditoriumID = Integer.parseInt(auditoriumIDField.getText());
+        Timestamp timestamp = Timestamp.valueOf(timestampField.getText());
+
+        if (connection.IsAddScreeningSuccessful(movieID, auditoriumID, timestamp)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successfully added!");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Something went wrong.");
+            alert.showAndWait();
+        }
     }
 }
--- src/sample/DatabaseConnection.java
+++ src/sample/DatabaseConnection.java
@@ -3,7 +3,12 @@ package sample;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.*;
+import java.time.LocalDate;
+
+import com.mysql.cj.protocol.Resultset;
 import org.mindrot.jbcrypt.BCrypt;
+
+import javax.xml.transform.Result;
 
 
 public class DatabaseConnection {
@@ -43,7 +48,7 @@ public class DatabaseConnection {
             }
 
         } catch (SQLException ex) {
-            System.err.print(ex);
+            System.err.println(ex);
         }
 
         return false;
@@ -69,11 +74,13 @@ public class DatabaseConnection {
 
             ResultSet resultSet = preparedStatement.executeQuery();
             resultSet.absolute(0);
+
             if (resultSet.next()) {
                 return true;
             }
+
         } catch (SQLException ex) {
-            System.err.print(ex);
+            System.err.println(ex);
         }
 
         return false;
@@ -102,7 +109,142 @@ public class DatabaseConnection {
             return true;
 
         } catch (SQLException ex) {
-            System.err.print(ex);
+            System.err.println(ex);
+        }
+
+        return false;
+    }
+
+    public ResultSet GetUserTable() {
+        String query = "SELECT * FROM user";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+
+        return null;
+    }
+
+    public ResultSet GetMovieTable() {
+        String query = "SELECT * FROM movie";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+
+        return null;
+    }
+
+    public ResultSet GetAuditoriumTable() {
+        String query = "SELECT * FROM auditorium";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+
+        return null;
+    }
+
+    public ResultSet GetScreeningTable() {
+        String query = "SELECT * FROM screening";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+
+        return null;
+    }
+
+    public boolean IsAddMovieSuccessful(String title, String director, String cast, String desc, String url, Date date, String duration) {
+        String query = "INSERT INTO movie(title, director, cast, description, " +
+                "movie_img_url, release_date, duration_min) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            if (title.isEmpty()) {
+                preparedStatement.setNull(1, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(1, title);
+            }
+
+            preparedStatement.setString(2, director);
+            preparedStatement.setString(3, cast);
+            preparedStatement.setString(4, desc);
+            preparedStatement.setString(5, url);
+            preparedStatement.setDate(6, date);
+            preparedStatement.setString(7, duration);
+
+            preparedStatement.executeUpdate();
+            return true;
+
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+
+        return false;
+    }
+
+    public boolean IsAddAuditoriumSuccessful(String name, String seatsNumber) {
+        String query = "INSERT INTO auditorium(name, seats_no) VALUES (?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            if (name.isEmpty()) {
+                preparedStatement.setNull(1, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(1, name);
+            }
+
+            if (seatsNumber.isEmpty()) {
+                preparedStatement.setNull(2, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(2, seatsNumber);
+            }
+
+            preparedStatement.executeUpdate();
+            return true;
+
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+
+        return false;
+    }
+
+    public boolean IsAddScreeningSuccessful(int movieID, int auditoriumID, Timestamp timestamp) {
+        String query = "INSERT INTO screening(movie_id, auditorium_id, screening_start)" +
+                "VALUES (?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, movieID);
+            preparedStatement.setInt(2, auditoriumID);
+            preparedStatement.setTimestamp(3, timestamp);
+
+            preparedStatement.executeUpdate();
+            return true;
+
+        } catch (SQLException ex) {
+            System.err.println(ex);
         }
 
         return false;
